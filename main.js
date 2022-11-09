46933b6e1b986aa7d3272400403d5a0b17e5bb77
Ivan Chipriyanov <ivanchipriqnov@gmail.com>
2022-11-09 19:48:03 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function menuClase() {
+    let siteNav = document.getElementById('site-nav');
+    siteNav.classList.toggle('site-nav-open');
+    let menuOpen = document.getElementById('menu-toggle');
+    menuOpen.classList.toggle('menu-open');
+
+}
